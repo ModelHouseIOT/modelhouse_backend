ef546f69c82e4856230ede40b4d3950b9a446a6d
DiegoPorta20 <u202019287@upc.edu.pe>
2023-06-26 13:40:28 -0500

    feat: Change swager

--- src/main/java/com/upc/modelhouse/ServiceManagement/api/ProjectActivityController.java
+++ src/main/java/com/upc/modelhouse/ServiceManagement/api/ProjectActivityController.java
@@ -26,26 +26,26 @@ public class ProjectActivityController {
         this.mapper = mapper;
     }
     @GetMapping("/proposal/{proposalId}/project_activity")
-    @Operation(tags = {"Proposal"})
+    @Operation(tags = {"project-activity"})
     @PreAuthorize("hasRole('ADMIN')or hasRole('USER')")
     public List<ProjectActivityDto> getAllByProposalId(@PathVariable("proposalId") Long proposalId){
         return mapper.listToResource(projectActivityService.findAllProposalId(proposalId));
     }
     @PostMapping("/proposal/{proposalId}/project_activity")
-    @Operation(tags = {"Proposal"})
+    @Operation(tags = {"project-activity"})
     @PreAuthorize("hasRole('ADMIN')or hasRole('USER')")
     public ProjectActivityDto create(@PathVariable("proposalId") Long proposalId,
                                             @RequestBody CreateProjectActivityDto createProjectActivityDto){
         return mapper.toResource(projectActivityService.create(proposalId, mapper.toModel(createProjectActivityDto)));
     }
     @PutMapping("/project_activity/{id}")
-    @Operation(tags = {"Proposal"})
+    @Operation(tags = {"project-activity"})
     @PreAuthorize("hasRole('ADMIN')or hasRole('USER')")
     public ProjectActivityDto update(@PathVariable("id") Long id, @RequestBody UpdateProjectActivityDto updateProjectActivityDto){
         return mapper.toResource(projectActivityService.update(id, mapper.toModel(updateProjectActivityDto)));
     }
     @DeleteMapping("/project_activity/{id}")
-    @Operation(tags = {"Proposal"})
+    @Operation(tags = {"project-activity"})
     @PreAuthorize("hasRole('ADMIN')or hasRole('USER')")
     public ResponseEntity<?> delete(@PathVariable("id") Long id){
         return  projectActivityService.delete(id);
