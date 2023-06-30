d4f7e6c21a0c97f281d3b6eb2fdc6f176a0bde08
DiegoPorta20 <u202019287@upc.edu.pe>
2023-06-30 07:44:44 -0500

    Update project activity resource

--- src/main/java/com/upc/modelhouse/ServiceManagement/resource/ProjectActivity/UpdateProjectActivityDto.java
+++ src/main/java/com/upc/modelhouse/ServiceManagement/resource/ProjectActivity/UpdateProjectActivityDto.java
@@ -1,5 +1,12 @@
 package com.upc.modelhouse.ServiceManagement.resource.ProjectActivity;
 
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateProjectActivityDto {
     private String status;
     private String name;
